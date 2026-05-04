2565481ffd3c5d00b5c0e492d9d25bfb70f0e1a7
Weien Xu <memeotonami@gmail.com>
2026-05-04 02:53:19 -0400

    Align battle character world bars

--- Ten Rings 2/Assets/Scripts/Battle/BattleWorldUnitBarsView.cs
+++ Ten Rings 2/Assets/Scripts/Battle/BattleWorldUnitBarsView.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7e3ae6c39975f3df7782c17d3960e1db5e68a45e5ec4ca0fd3f0b338270f9f72
+oid sha256:2ea8ec34dddfde380af3d697837a1d6d58cf1b45a5edb5bfbfad7f5ec721553d
 size 17106
